b1a7f60c54195b81de71918d0fb3231b91e6c35c
Ndriçim Sadiku - Frakton <nsadiku@hotmail.de>
2020-06-29 21:49:05 +0200

    Choose Topic Activity improved and Snackbar #added to show user a hint if there is no topic selected

--- app/src/main/java/com/bmn/bookfinder/activities/ChooseTopicsActivity.java
+++ app/src/main/java/com/bmn/bookfinder/activities/ChooseTopicsActivity.java
@@ -5,36 +5,29 @@ import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.GridLayoutManager;
 
 import com.bmn.bookfinder.R;
 import com.bmn.bookfinder.adapters.ChooseTopicsLayoutAdapter;
 import com.bmn.bookfinder.databinding.ActivityChooseTopicsBinding;
-import com.bmn.bookfinder.models.Topic;
+import com.bmn.bookfinder.dummydata.DummyData;
+import com.bmn.bookfinder.helpers.SharedPrefUtils;
 import com.bmn.bookfinder.utils.DimenUtils;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.google.android.material.snackbar.Snackbar;
 
 public class ChooseTopicsActivity extends AppCompatActivity implements View.OnClickListener {
-
-   private ActivityChooseTopicsBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_choose_topics);
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_choose_topics);
-        binding.topicsGrid.setLayoutManager(new GridLayoutManager(getApplicationContext(), 3));
+        ActivityChooseTopicsBinding binding = ActivityChooseTopicsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
+        binding.topicsGrid.setLayoutManager(new GridLayoutManager(getApplicationContext(), 3));
         DimenUtils.setTopBottomPadding(binding.getRoot());
-        List<Topic> topics = new ArrayList<>();
-        for (int i = 0; i < 40; i++) {
-            topics.add(new Topic("Topic", R.drawable.history_topic));
-        }
+
         ChooseTopicsLayoutAdapter chooseTopicsLayoutAdapter = new ChooseTopicsLayoutAdapter(
-                getApplicationContext(), topics
+                getApplicationContext(), DummyData.getFirstUseTopics()
         );
 
         binding.topicsGrid.setAdapter(chooseTopicsLayoutAdapter);
@@ -43,6 +36,11 @@ public class ChooseTopicsActivity extends AppCompatActivity implements View.OnCl
 
     @Override
     public void onClick(View v) {
-        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        if (!SharedPrefUtils.getFavoriteIds(getApplicationContext()).isEmpty()) {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            SharedPrefUtils.storeFirstTimeUsedTimestamp(getApplicationContext(), System.currentTimeMillis());
+        } else {
+            Snackbar.make(findViewById(android.R.id.content), R.string.please_choose_topics, Snackbar.LENGTH_SHORT).show();
+        }
     }
 }
