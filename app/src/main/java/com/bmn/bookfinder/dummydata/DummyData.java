ac6c9dd1bfa6265ed6ce325e0fd07357f1669b99
Ndriçim Sadiku - Frakton <nsadiku@hotmail.de>
2020-06-29 21:47:27 +0200

    Topics added as dummy data

--- app/src/main/java/com/bmn/bookfinder/dummydata/DummyData.java
+++ app/src/main/java/com/bmn/bookfinder/dummydata/DummyData.java
@@ -24,4 +24,27 @@ public class DummyData {
         return dummyTopics;
     }
 
+    public static ArrayList<Topic> getFirstUseTopics() {
+        ArrayList<Topic> dummyTopics = new ArrayList<>();
+        dummyTopics.add(new Topic("Biography", "http://books.google.com/books/content?id=0xqrU5lnD7AC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"));
+        dummyTopics.add(new Topic("Business", "http://books.google.com/books/content?id=z5RS9uT1hkIC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"));
+        dummyTopics.add(new Topic("Children", "http://books.google.com/books/content?id=noJMA1rqLjUC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"));
+        dummyTopics.add(new Topic("Cookery", "http://books.google.com/books/content?id=qLYPB76dIvMC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"));
+        dummyTopics.add(new Topic("Fiction", "http://books.google.com/books/content?id=AHqxx2Xw3qsC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"));
+        dummyTopics.add(new Topic("Satire", "https://books.google.com/books/content?id=mGcAAAAAYAAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"));
+        dummyTopics.add(new Topic("Drama", "http://books.google.com/books/content?id=Tx1oU0pEt_0C&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api"));
+        dummyTopics.add(new Topic("Action", "http://books.google.com/books/content?id=CNHrJrRbrPsC&printsec=frontcover&img=1&zoom=1&source=gbs_api"));
+        dummyTopics.add(new Topic("Adventure", "http://books.google.com/books/content?id=SXlf834V0V4C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"));
+        dummyTopics.add(new Topic("Travel", "http://books.google.com/books/content?id=uPwsDgAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"));
+        dummyTopics.add(new Topic("Religious", "http://books.google.com/books/content?id=sOUBiAPxfigC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"));
+        dummyTopics.add(new Topic("Science", "http://books.google.com/books/content?id=8YLrBwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"));
+        dummyTopics.add(new Topic("History", "http://books.google.com/books/content?id=iWnbAAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"));
+        dummyTopics.add(new Topic("Math", "http://books.google.com/books/content?id=bhh9pNtNfWsC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"));
+        dummyTopics.add(new Topic("Poetry", "http://books.google.com/books/content?id=bl0WDAAAQBAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"));
+        dummyTopics.add(new Topic("Anthologies", "http://books.google.com/books/content?id=I1L3GW81bM4C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"));
+        dummyTopics.add(new Topic("Encyclopedia", "http://books.google.com/books/content?id=7CAjAQAAIAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"));
+        dummyTopics.add(new Topic("Dictionaries", "http://books.google.com/books/content?id=rLcjqEUzabYC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"));
+        return dummyTopics;
+    }
+
 }
