c4d692f3c45999722a8990cc6bf97d7cd7f9e453
Miran Mustafa <miran.mustafa@outlook.com>
2020-06-30 09:52:23 +0200

    Added viewpager for tabed account activity

---
+++ app/src/main/java/com/bmn/bookfinder/adapters/CreateAccountViewPagerAdapter.java
@@ -0,0 +1,34 @@
+package com.bmn.bookfinder.adapters;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CreateAccountViewPagerAdapter extends FragmentStatePagerAdapter {
+
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+
+
+    public CreateAccountViewPagerAdapter(FragmentManager fragment) {
+        super(fragment, FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    public void addFragment(Fragment fragment) {
+        mFragmentList.add(fragment);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+}
