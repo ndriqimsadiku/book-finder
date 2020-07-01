aef06e3f5650fd056c0ccea1173ce5b235bbb5d8
Ndriçim Sadiku - Frakton <nsadiku@hotmail.de>
2020-07-01 22:11:53 +0200

    Favorite Topics #done

--- app/src/main/java/com/bmn/bookfinder/adapters/BindingAdapter.kt
+++ app/src/main/java/com/bmn/bookfinder/adapters/BindingAdapter.kt
@@ -3,6 +3,7 @@ package com.bmn.bookfinder.adapters
 import android.widget.ImageView
 import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
 import com.google.android.material.card.MaterialCardView
 
 @BindingAdapter("cardBackground")
@@ -19,4 +20,12 @@ fun imageResource(
     resource: Int
 ) {
     imageView.setImageResource(resource)
+}
+
+@BindingAdapter("glideUrl")
+fun imageResource(
+    imageView: ImageView,
+    url:String
+) {
+    Glide.with(imageView.context).load(url).into(imageView)
 }
