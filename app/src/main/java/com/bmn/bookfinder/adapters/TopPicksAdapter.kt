a5ac78628d1a712b51faab45f750a69850f26c67
Miran Mustafa <miranmustafaa@gmail.com>
2021-05-18 22:22:58 +0200

    Adapters migrated to kotlin

---
+++ app/src/main/java/com/bmn/bookfinder/adapters/TopPicksAdapter.kt
@@ -0,0 +1,67 @@
+package com.bmn.bookfinder.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bmn.bookfinder.R
+import com.bmn.bookfinder.data.room.BookEntity
+import com.bumptech.glide.Glide
+
+class TopPicksAdapter(private val context: Context, topPicks: List<BookEntity>) :
+    RecyclerView.Adapter<TopPicksAdapter.ViewHolder>() {
+    private val topPicks: List<BookEntity>
+    private val mInflater: LayoutInflater
+    private var mClickListener: ItemClickListener? = null
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = mInflater.inflate(R.layout.top_picks_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.text.text = topPicks[position].title
+        Glide.with(context)
+            .load(topPicks[position].thumbnailUrl)
+            .into(holder.image)
+    }
+
+    override fun getItemCount(): Int {
+        return topPicks.size
+    }
+
+    fun setClickListener(itemClickListener: ItemClickListener?) {
+        mClickListener = itemClickListener
+    }
+
+    // parent activity will implement this method to respond to click events
+    interface ItemClickListener {
+        fun onItemClick(view: View?, id: String?)
+    }
+
+    // stores and recycles views as they are scrolled off screen
+    inner class ViewHolder internal constructor(itemView: View) : RecyclerView.ViewHolder(itemView),
+        View.OnClickListener {
+        var text: TextView
+        var image: ImageView
+        override fun onClick(view: View) {
+            if (mClickListener != null) mClickListener!!.onItemClick(
+                view,
+                topPicks[adapterPosition].id
+            )
+        }
+
+        init {
+            text = itemView.findViewById(R.id.top_picks_text)
+            image = itemView.findViewById(R.id.top_picks_image)
+            itemView.setOnClickListener(this)
+        }
+    }
+
+    init {
+        mInflater = LayoutInflater.from(context)
+        this.topPicks = topPicks
+    }
+}
---
+++ app/src/main/java/com/bmn/bookfinder/adapters/TopicBooksAdapter.kt
@@ -0,0 +1,76 @@
+package com.bmn.bookfinder.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bmn.bookfinder.R
+import com.bmn.bookfinder.data.room.BookEntity
+import com.bumptech.glide.Glide
+import java.util.*
+
+/**
+ * Created by Ndriçim Sadiku on 01 July 2020
+ * ndricim@frakton.com
+ */
+class TopicBooksAdapter(private val mContext: Context) :
+    RecyclerView.Adapter<TopicBooksAdapter.ViewHolder>() {
+    private var books: List<BookEntity>
+    private var listener: OnItemTopicBookClick? = null
+    fun setBooks(topics: List<BookEntity>) {
+        books = topics
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_list_book, parent, false),
+            listener
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val book = books[position]
+        holder.title.text = book.title
+        holder.description.text = book.description
+        Glide.with(mContext).load(book.thumbnailUrl).into(holder.thumbnail)
+    }
+
+    override fun getItemCount(): Int {
+        return books.size
+    }
+
+    fun setListener(listener: OnItemTopicBookClick?) {
+        this.listener = listener
+    }
+
+    interface OnItemTopicBookClick {
+        fun onItemTopicBookClick(view: View?, bookId: String?)
+    }
+
+    inner class ViewHolder(itemView: View, listener: OnItemTopicBookClick?) :
+        RecyclerView.ViewHolder(itemView) {
+        val title: TextView
+        val thumbnail: ImageView
+        val description: TextView
+
+        init {
+            title = itemView.findViewById(R.id.book_title)
+            description = itemView.findViewById(R.id.book_description)
+            thumbnail = itemView.findViewById(R.id.book_thumbnail)
+            itemView.setOnClickListener { v: View? ->
+                listener?.onItemTopicBookClick(
+                    itemView,
+                    books[adapterPosition].id
+                )
+            }
+        }
+    }
+
+    init {
+        books = ArrayList()
+    }
+}
